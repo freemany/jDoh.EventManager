eb3de04b93dc7775158d71242e82f16c2e4f8dfc
freemany <freeman_yam@hotmail.com>
2019-05-01 18:27:51 +1000

    reverse event-manager.js back to Promise

--- src/event-manager.js
+++ src/event-manager.js
@@ -1,20 +1,17 @@
-const $ = require('jquery-deferred');
-
 var EventManager = (function() {
     var _instance;
 
     function em() {
         this.events = {};
-        this.channels = {};
         this.pubSubPromise = {};
 
         this.publish = function(key, item) {
             if (undefined === this.pubSubPromise[key] || true === this.pubSubPromise[key]['published']) {
-                this.pubSubPromise[key] = {}; 
-                var defer = $.Deferred();
-                this.pubSubPromise[key]['r'] = defer.resolve;
-                defer.resolve(item);
-                this.pubSubPromise[key]['p'] = defer.promise();
+                this.pubSubPromise[key] = {};
+                this.pubSubPromise[key]['p'] = new Promise((resolve) => {
+                    this.pubSubPromise[key]['r'] = resolve;
+                    resolve(item);
+                })
             } else {
                 this.pubSubPromise[key]['r'](item);
             }
@@ -22,79 +19,79 @@ var EventManager = (function() {
             this.pubSubPromise[key]['published'] = true;
         }
 
-        this.subscribe = function(key, callback) { 
-             if (typeof callback !== 'function') {
+        this.subscribe = function(key, callback) {
+            if (typeof callback !== 'function') {
                 return null;
-             }
+            }
 
-             if (undefined === this.pubSubPromise[key]) {
-                this.pubSubPromise[key] = {}; 
-                var defer = $.Deferred();
-                this.pubSubPromise[key]['r'] = defer.resolve;
-                this.pubSubPromise[key]['p'] = defer.promise();
-            } 
+            if (undefined === this.pubSubPromise[key]) {
+                this.pubSubPromise[key] = {};
+                this.pubSubPromise[key]['p'] = new Promise((resolve) => {
+                    this.pubSubPromise[key]['r'] = resolve;
+                })
+            }
             this.pubSubPromise[key]['p'].then(callback);
         }
 
         this.on = function(key, callback) {
-                    if (undefined === this.events[key]) {
-                        this.events[key] = [];
-                    }
-                    this.events[key].push(callback);
+            if (undefined === this.events[key]) {
+                this.events[key] = [];
+            }
+            this.events[key].push(callback);
 
-                    return true;
-                };
+            return true;
+        };
 
         this.trigger = function(key, params) {
-                    if (undefined === this.events[key]) {
-                        return false;
-                    }
+            if (undefined === this.events[key]) {
+                return false;
+            }
 
-                    for(var i=0; i < this.events[key].length; i++) {
-                        var shortCurcuit = this.events[key][i].apply(this, params);
-                        if (true === shortCurcuit) {
-                            return true;
-                        }
-                    }
-                    return;
+            for(var i=0; i < this.events[key].length; i++) {
+                var shortCurcuit = this.events[key][i].apply(this, params);
+                if (true === shortCurcuit) {
+                    return true;
                 }
+            }
+            return;
+        }
+    }
+
+    function getInstance() {
+        if (undefined === _instance) {
+            _instance = new em();
         }
 
-        function getInstance() {
-                if (undefined === _instance) {
-                    _instance = new em();
-                }
+        return _instance;
+    }
 
-                return _instance;
-        }
+    function on(key, callback) {
+        return getInstance().on(key, callback);
+    }
 
-        function on(key, callback) {
-                return getInstance().on(key, callback);
-        }
+    function trigger(key, params) {
+        return getInstance().trigger(key, params);
+    }
 
-        function trigger(key, params) {
-                return getInstance().trigger(key, params);
-        }
+    function publish(key, item) {
+        return getInstance().publish(key, item);
+    }
 
-        function publish(key, item) {
-            return getInstance().publish(key, item);
-        }
+    function subscribe(key, callback) {
+        return getInstance().subscribe(key, callback);
+    }
 
-        function subscribe(key, callback) {
-            return getInstance().subscribe(key, callback);
-        }
-
-        return {
-                getInstance: getInstance,
-                on: on,
-                attach: on,
-                trigger: trigger,
-                emit: trigger,
-                publish: publish,
-                pub: publish,
-                subscribe: subscribe,
-                sub: subscribe,
-        }
+    return {
+        getInstance: getInstance,
+        on: on,
+        attach: on,
+        trigger: trigger,
+        emit: trigger,
+        publish: publish,
+        pub: publish,
+        subscribe: subscribe,
+        sub: subscribe,
+    }
 })();
 
 module.exports = EventManager;
